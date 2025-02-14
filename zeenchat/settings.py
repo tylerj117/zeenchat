da99f3ce76228037c60abfd51f102c0320303e3a
Tyler J <tyler@jackfruit.me>
2025-02-14 13:55:38 -0500

    Adds healthcheck

--- zeenchat/settings.py
+++ zeenchat/settings.py
@@ -55,6 +55,7 @@ ASGI_APPLICATION = 'zeenchat.asgi.application'
 
 
 MIDDLEWARE = [
+    'chatapp.middleware.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
